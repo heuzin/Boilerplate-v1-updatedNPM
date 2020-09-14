9dd8ee8fa9bedfb09b689efa16b29ca81405e4b6
heuzin <matheusqs.arq@gmail.com>
2020-09-14 06:20:42 -0400

    .babelrc file and first Hello message showing on screen

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+class HelloMessage extends React.Component {
+    render() {
+      return (
+        <div>
+          Hello 
+        </div>
+      );
+    }
+  }
+  
+  ReactDOM.render(
+    <HelloMessage/>,
+    document.getElementById('app')
+);
