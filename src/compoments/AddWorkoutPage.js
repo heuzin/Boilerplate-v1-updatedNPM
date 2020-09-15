b34d066bd4f52d85fa3dd82d5ab2c0429946c5e4
heuzin <matheusqs.arq@gmail.com>
2020-09-15 11:27:44 -0400

    Workout form

---
+++ src/compoments/AddWorkoutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import WorkoutForm from './WorkoutForm'
+
+class AddWorkoutPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <WorkoutForm />
+            </div>
+        )
+    }
+}
+
+export default AddWorkoutPage;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Router, Switch } from 'react-router-dom';
 import WorkoutDashboardPage from '../compoments/WorkoutDashboardPage';
+import AddWorkoutPage from '../compoments/AddWorkoutPage'
 import createHistory from 'history/createBrowserHistory';
 
 export const history = createHistory();
@@ -10,6 +11,7 @@ const AppRouter = () => (
         <div>
             <Switch>
                 <Route path='/dashboard' component={WorkoutDashboardPage}/>
+                <Route path='/create' component={AddWorkoutPage}/>
             </Switch>
         </div>
     </Router>
