c2900d90e2cc0a79189320c672115084a0ea02f4
heuzin <matheusqs.arq@gmail.com>
2020-09-16 17:50:49 -0400

    Reducer Workout

---
+++ src/reducers/workout.js
@@ -0,0 +1,29 @@
+// Workout reducer
+const workoutReducerDefaultState = [];
+
+const workoutReducer = (state = workoutReducerDefaultState, action) => {
+    switch (action.type) {
+        case 'ADD_WORKOUT':
+            return [
+                ...state,
+                action.workout
+            ]
+        case 'REMOVE_WORKOUT':
+            return state.filter(({ id }) => id !== action.id);
+        case 'EDIT_WORKOUT':
+            return state.map((workout) => {
+                if (workout.id === action.id) {
+                    return {
+                        ...workout,
+                        ...action.updates
+                    }
+                } else {
+                    return workout
+                }
+            })
+        default:
+            return state
+    }
+}
+
+export default workoutReducer
