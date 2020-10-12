dd11048bcea36d3a53156d5ce707ec38a71b856c
heuzin <matheusqs.arq@gmail.com>
2020-10-12 10:33:31 -0400

    setting up state

--- src/app.js
+++ src/app.js
@@ -1,5 +1,33 @@
-const template = <p>Workout</p>
+class WorkoutApp extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            exercise: []
+        }
+    }
+    render() {
+        const title = 'Workout app'
+        return (
+            <div>
+                <Header title={title}/>
+                <ExerciseName exercise={this.state.exercise}/>
+            </div>
+        )
+    }
+}
 
-const appRoot = document.getElementById('app')
+const Header = (props) => {
+    return (
+        <h1>{props.title}</h1>
+    )
+}
 
-ReactDOM.render(template,appRoot)
+const ExerciseName = (props) => {
+    return (
+        props.exercise.map((exercise) => <p key={exercise}>{exercise}</p>)
+    )
+}
+
+let appRoot = document.getElementById('app');
+
+ReactDOM.render(<WorkoutApp />, appRoot);
