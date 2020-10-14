df83b13663cc6b522c920c5be7d1cb0220f83e30
heuzin <matheusqs.arq@gmail.com>
2020-10-14 15:42:20 -0400

    scss configuration on webpack,config.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import WorkoutApp from './components/WorkoutApp'
+import WorkoutApp from './components/WorkoutApp';
+import './styles/styles.scss';
 
 let appRoot = document.getElementById('app');
 
