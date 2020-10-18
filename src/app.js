f788a133f5fcc71869cf11b3b1445599b85fe034
heuzin <matheusqs.arq@gmail.com>
2020-10-18 14:06:42 -0400

    Boilerplate with updated npm

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import WorkoutApp from './components/WorkoutApp';
+import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-let appRoot = document.getElementById('app');
-
-ReactDOM.render(<WorkoutApp />, appRoot);
+ReactDOM.render(<p>This is a boilerplate.</p>, document.getElementById('app'));
